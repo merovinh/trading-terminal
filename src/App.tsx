1978361354732c1ab2fa034b389eccf3473a9c58
Elumion <dovgalyukvlad87@gmail.com>
2022-04-01 16:38:39 +0300

    add ccxt as dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+// import * as ccxt from "ccxt";
+
 function App() {
+
+useEffect(()=>{
+  // console.log(ccxt.exchanges);
+  console.log ((window as any).ccxt.exchanges); // print all available exchanges
+});
+
+
   return (
     <div className="App">
+      fwgew
     </div>
   );
 }
