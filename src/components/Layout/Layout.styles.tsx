f76d2b1a06c15c2330e00cd22d3bbde4b7b901ec
Elumion <dovgalyukvlad87@gmail.com>
2022-04-02 16:05:33 +0300

    style update

--- src/components/Layout/Layout.styles.tsx
+++ src/components/Layout/Layout.styles.tsx
@@ -2,18 +2,30 @@ import styled from "@emotion/styled";
 
 export const Header = styled.div`
   width: 100%;
-  padding: 15px;
-  background-color: magenta;
+  padding: 10px;
+  background-color: #1f1f1f;
 `;
 export const BtnLink = styled.span`
   a {
     color: #fff;
+    transition: color 250ms ease;
+    transition: text-shadow 250ms ease;
+    &:hover {
+      color: #c8f7f5;
+      text-shadow: 1px 2px 2px black;
+    }
+  }
+  transition: all 200ms ease;
+  &:hover {
+    transform: scale(1.2);
   }
 `;
 export const Navigation = styled.nav`
   width: fit-content;
   display: flex;
   gap: 40px;
-  background-color: black;
+  background-color: #303030;
+  padding: 10px;
+  border-radius: 5px;
   justify-content: space-between;
 `;
