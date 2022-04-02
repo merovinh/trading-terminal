2cb269742872fa7eb5a1b01460033eb44b059520
Elumion <dovgalyukvlad87@gmail.com>
2022-04-02 15:00:18 +0300

    global styles

---
+++ src/Global.styles.tsx
@@ -0,0 +1,37 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyles = css`
+  *,
+  ::before,
+  ::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body,
+  #root {
+    min-height: 100%;
+  }
+
+  body {
+    margin: 0;
+    font-family: "Poppins", sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    font-weight: normal;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
