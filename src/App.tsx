635decd223e645cfd499b85dd3ae893c8002a53a
Elumion <dovgalyukvlad87@gmail.com>
2022-04-02 14:59:42 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
 import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Balances } from "./pages/Balances";
+import { Exchanges } from "./pages/Exchanges";
+import { Terminal } from "./pages/Terminal";
 import * as ccxt from "ccxt";
+import { Layout } from "./components/Layout";
+import { Global } from "@emotion/react";
+import { GlobalStyles } from "./Global.styles";
 
 function App() {
   const ccxt = (window as any).ccxt;
 
-useEffect(()=>{
-  console.log (ccxt.exchanges); // print all available exchanges
-  let kucoin:any = new ccxt.kucoin();
-});
+  useEffect(() => {
+    console.log(ccxt.exchanges); // print all available exchanges
+    let kucoin: any = new ccxt.kucoin();
+  });
 
   return (
     <div className="App">
+      <Global styles={GlobalStyles}></Global>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Navigate replace to="/Terminal" />}></Route>
+          <Route
+            path="/terminal"
+            element={
+              <Layout>
+                <Terminal />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path="/exchanges"
+            element={
+              <Layout>
+                <Exchanges />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path="/balances"
+            element={
+              <Layout>
+                <Balances />
+              </Layout>
+            }
+          ></Route>
+          <Route path="/" element={<Navigate replace to="/Terminal" />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
