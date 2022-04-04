4f0025b905019d8365ace4aae61bd7347dfd7dc1
Elumion <dovgalyukvlad87@gmail.com>
2022-04-04 17:26:45 +0300

    minor commit

--- public/main.js
+++ public/main.js
@@ -53,39 +53,39 @@ catch (e) { alert('Failed to save the file !'); }
 //     console.log("ConneCted");
 // })
 
-const net = require("net");
+// const net = require("net");
 
 // Create a simple server
-var server = net.createServer(function (conn) {
-    console.log("Server: Client connected");
+// var server = net.createServer(function (conn) {
+//     console.log("Server: Client connected");
 
-    // If connection is closed
-    conn.on("end", function () {
-        console.log('Server: Client disconnected');
-        // Close the server
-        server.close();
-        // End the process
-        process.exit(0);
-    });
+//     // If connection is closed
+//     conn.on("end", function () {
+//         console.log('Server: Client disconnected');
+//         // Close the server
+//         server.close();
+//         // End the process
+//         process.exit(0);
+//     });
 
-    // Handle data from client
-    conn.on("data", function (data) {
-        data = JSON.parse(data);
-        console.log("Response from client: %s", data.response);
-    });
+//     // Handle data from client
+//     conn.on("data", function (data) {
+//         data = JSON.parse(data);
+//         console.log("Response from client: %s", data.response);
+//     });
 
-    // Let's response with a hello message
-    conn.write(
-        JSON.stringify(
-            { response: "Hey there client!" }
-        )
-    );
-});
+//     // Let's response with a hello message
+//     conn.write(
+//         JSON.stringify(
+//             { response: "Hey there client!" }
+//         )
+//     );
+// });
 
-// Listen for connections
-server.listen(3000, "localhost", function () {
-    console.log("Server: Listening");
-});
+// // Listen for connections
+// server.listen(3000, "localhost", function () {
+//     console.log("Server: Listening");
+// });
 
 
 
