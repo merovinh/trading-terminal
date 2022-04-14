691cc68acf87d6d44dbe5b76c37204e3b1ea5068
Elumion <dovgalyukvlad87@gmail.com>
2022-04-14 17:21:47 +0300

    orders style

--- src/components/OrdersList/OrdersList.styles.tsx
+++ src/components/OrdersList/OrdersList.styles.tsx
@@ -1,1 +1,85 @@
 import styled from "@emotion/styled";
+
+export const OrdersContainer = styled.div`
+  width: 100%;
+  background-color: #242424;
+  div {
+    padding: 10px;
+    background-color: #202020;
+    p {
+      font-weight: 800;
+      letter-spacing: 1px;
+      font-size: 13px;
+    }
+  }
+  ul {
+    height: calc(100vh - 56px - 57px);
+    overflow-y: scroll;
+    ::-webkit-scrollbar-track {
+      border: 1px solid transparent;
+      padding: 2px 0;
+      background-color: transparent;
+    }
+
+    ::-webkit-scrollbar {
+      width: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+      background-color: #f1f1f1;
+      border: 1px solid #f1f1f1;
+      transform: rotate(180deg);
+    }
+  }
+  div,
+  li {
+    /* background-color: black; */
+    color: #fff;
+    width: 100%;
+    display: grid;
+    grid-template-areas: "A A B C D F F G G J J L";
+    gap: 10px;
+    grid-template-columns: 180px 100px 50px 70px repeat(4, 1fr) 50px;
+  }
+  p:nth-of-type(1n) {
+    grid-area: "A";
+  }
+  p:nth-of-type(2n) {
+    grid-area: "B";
+  }
+  p:nth-of-type(3n) {
+    grid-area: "C";
+  }
+  p:nth-of-type(4n) {
+    grid-area: "D";
+  }
+  p:nth-of-type(5n) {
+    grid-area: "F";
+  }
+  p:nth-of-type(6n) {
+    grid-area: "G";
+  }
+  p:nth-of-type(7n) {
+    grid-area: "J";
+  }
+  p:nth-of-type(8n) {
+    grid-area: "L";
+  }
+
+  li:nth-of-type(1n) {
+  }
+  li:nth-of-type(2n) {
+    background-color: #232c2d;
+  }
+  li {
+    padding: 10px;
+    :hover {
+      background-color: #434b52;
+    }
+  }
+`;
+export const OrdersItem = styled.li`
+  font-size: 14px;
+`;
--- src/components/OrdersList/OrdersList.tsx
+++ src/components/OrdersList/OrdersList.tsx
@@ -1,16 +1,54 @@
+import { OrdersContainer, OrdersItem } from "./OrdersList.styles";
+
 const OrdersList = ({ ordersArray }: any) => {
   const renderOrders = () => {
     return ordersArray.map((elem: any) => (
-      <li
+      <OrdersItem
         key={elem.id}
         data-key={elem.id}
-        onClick={(e: any) => console.log(e.target.dataset.key)}
+        // onClick={(e: any) => console.log(e.target.dataset.key)}
       >
-        {elem.symbol}
-      </li>
+        <p>{elem.datetime}</p>
+        <p>{elem.symbol}</p>
+        <p>{elem.type}</p>
+        <p>{elem.side}</p>
+        <p>{elem.price}</p>
+        <p>{elem.amount}</p>
+        <p>{elem.filled}</p>
+        <p>{elem.remaining}</p>
+        <button>Cansel</button>
+      </OrdersItem>
     ));
   };
 
-  return <ul>{renderOrders()}</ul>;
+  return (
+    <OrdersContainer style={{ width: "100%" }}>
+      <div>
+        <p>Time</p>
+        <p>Pair</p>
+        <p>Type</p>
+        <p>Buy/Sell</p>
+        <p>Price</p>
+        <p>Amount</p>
+        <p>Filled</p>
+        <p>Unfilled</p>
+        <p>-</p>
+      </div>
+      <ul>
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+        {renderOrders()}
+      </ul>
+    </OrdersContainer>
+  );
 };
 export default OrdersList;
