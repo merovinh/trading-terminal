8c6ba6d1115e9dfd26d986f4f86b6a5801848b2a
Elumion <dovgalyukvlad87@gmail.com>
2022-04-25 18:27:17 +0300

    add component for store access

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 const container = document.getElementById("root")!;
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
