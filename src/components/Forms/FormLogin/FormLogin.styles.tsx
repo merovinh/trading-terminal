037566e5b614094a5eb2970d8c738e0408112de3
Elumion <dovgalyukvlad87@gmail.com>
2022-04-28 19:04:03 +0300

    add styles and components

--- src/components/Forms/FormLogin/FormLogin.styles.tsx
+++ src/components/Forms/FormLogin/FormLogin.styles.tsx
@@ -2,24 +2,53 @@ import styled from "@emotion/styled";
 
 export const FormContainer = styled.div`
   display: grid;
-  width: 500px;
+  width: 100%;
+  height: 100%;
+  grid-template-areas:
+    "input1 input1"
+    "input2 input3"
+    "input4 input5"
+    "btn-submit btn-submit";
+  grid-template-columns: 1fr 1fr;
+  padding: 10px 15px;
+  gap: 15px;
+  justify-items: center;
   div {
-    height: 50px;
     width: 100%;
   }
   div:nth-of-type(1) {
-    background-color: red;
+    grid-area: input1;
   }
   div:nth-of-type(2) {
-    background-color: blue;
+    grid-area: input2;
   }
   div:nth-of-type(3) {
-    background-color: yellow;
+    grid-area: input3;
   }
   div:nth-of-type(4) {
-    background-color: gray;
+    grid-area: input4;
   }
   div:nth-of-type(5) {
-    background-color: orange;
+    grid-area: input5;
+  }
+  button {
+    grid-area: btn-submit;
+    width: 300px;
+    padding: 5px 10px;
+    font-size: 18px;
+    font-weight: 900;
+    border: 1px solid #fff;
+    border-radius: 3px;
+    background-color: #6fe962;
+    transition: box-shadow 200ms ease, background-color 100ms ease;
+    cursor: pointer;
+    :hover {
+      background-color: #329927;
+      color: #f2f2f2;
+      box-shadow: 0 3px 0 black;
+    }
+    :active {
+      transform: scale(0.98);
+    }
   }
 `;
--- src/components/Forms/FormLogin/FormLogin.tsx
+++ src/components/Forms/FormLogin/FormLogin.tsx
@@ -1,13 +1,26 @@
+import { CustomInput } from "./CustomInput";
+import { CustomSelect } from "./CustomSelect";
 import { FormContainer } from "./FormLogin.styles";
 
 const FormLogin = () => {
   return (
     <FormContainer>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <CustomSelect />
+      </div>
+      <div>
+        <CustomInput label={"Name"} />
+      </div>
+      <div>
+        <CustomInput label={"API Key"} />
+      </div>
+      <div>
+        <CustomInput label={"API Secret"} />
+      </div>
+      <div>
+        <CustomInput label={"API Password"} />
+      </div>
+      <button>Submit</button>
     </FormContainer>
   );
 };
