4bcd99e3cf33e42a916e23d33ff1bd1eb5281595
Elumion <dovgalyukvlad87@gmail.com>
2022-05-09 19:15:31 +0300

    done balances page

--- src/pages/Balances/Balances.tsx
+++ src/pages/Balances/Balances.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BalancesList } from "../../components/BalancesList.tsx";
+import { fetchBalance } from "../../redux/balanceReducer";
+
 const Balances = () => {
+  const dispatch = useDispatch();
+  const balances: any = useSelector((state: any) => state.balance.data);
+  const exchange = useSelector((state: any) => state.SelectedExchange.data);
+
+  useEffect(() => {
+    dispatch(fetchBalance(exchange));
+    console.log("1: ", balances);
+  }, []);
+  useEffect(() => {
+    dispatch(fetchBalance(exchange));
+    console.log(balances);
+  }, [exchange]);
+
   return (
     <>
-      <div>True</div>
+      <BalancesList balancesArray={balances?.info.data} />
     </>
   );
 };
