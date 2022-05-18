f578b7194de1a28d25979a7823d8833250e10264
Elumion <dovgalyukvlad87@gmail.com>
2022-05-18 19:19:25 +0300

    add interval that reload orders every 2 min

--- src/components/Layout/ReloadBtn/ReloadBtn.tsx
+++ src/components/Layout/ReloadBtn/ReloadBtn.tsx
@@ -1,14 +1,16 @@
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { fetchBalance } from '../../../redux/balanceReducer';
 import { fetchCoins } from '../../../redux/coinsReducer';
 import { fetchExchanges } from '../../../redux/exchangesReducer';
-import { fetchOrders } from '../../../redux/ordersReducer';
+import { addHandOrders, fetchOrders } from '../../../redux/ordersReducer';
 import { Reload } from './ReloadBtn.styles';
 
 const ReloadBtn = () => {
     const ccxt = (window as any).ccxt;
     let kucoin: any = useSelector((state: any) => state.SelectedExchange.data);
+    let coins = useSelector((state: any) => state.coins.data);
 
     const dispatch = useDispatch();
     const handleReload = () => {
@@ -18,6 +20,16 @@ const ReloadBtn = () => {
         dispatch(fetchExchanges());
         toast.success('Reloaded!');
     };
+
+    const reloadOrders = async (exchange: any) => {
+        const orders = await exchange.fetchOpenOrders();
+        dispatch(addHandOrders(orders));
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(() => reloadOrders(kucoin), 120000);
+        return () => clearInterval(intervalId);
+    }, [kucoin]);
 
     return <Reload onClick={handleReload}>↻</Reload>;
 };
--- src/redux/ordersReducer.tsx
+++ src/redux/ordersReducer.tsx
@@ -1,34 +1,54 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchOrders: any = createAsyncThunk<any>(
-  "orders/fetchOrders",
-  async (exchange: any, { rejectWithValue }) => {
-    try {
-      // exchange.setSandboxMode(true); //=============
-      const response = await exchange.fetchOpenOrders();
-      return response;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data);
-    }
-  }
+    'orders/fetchOrders',
+    async (exchange: any, { rejectWithValue }) => {
+        try {
+            // exchange.setSandboxMode(true); //=============
+            const response = await exchange.fetchOpenOrders();
+            return response;
+        } catch (error: any) {
+            return rejectWithValue(error.response.data);
+        }
+    },
+);
+
+export const addHandOrders: any = createAsyncThunk<any>(
+    'orders/addOrders',
+    async (orders, { rejectWithValue }) => {
+        try {
+            // exchange.setSandboxMode(true); //=============
+            return orders;
+        } catch (error: any) {
+            return rejectWithValue(error.response.data);
+        }
+    },
 );
 
 const initialState = {};
 
 const ordersSlice: any = createSlice<any, any, any>({
-  name: "orders",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchOrders.fulfilled, (state, action) => {
-      state.data = action.payload;
-      state.status = "fulfilled";
-    });
-    builder.addCase(fetchOrders.rejected, (state, action) => {
-      state.data = action.payload;
-      state.status = "rejected";
-    });
-  },
+    name: 'orders',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchOrders.fulfilled, (state, action) => {
+            state.data = action.payload;
+            state.status = 'fulfilled';
+        });
+        builder.addCase(fetchOrders.rejected, (state, action) => {
+            state.data = action.payload;
+            state.status = 'rejected';
+        });
+        builder.addCase(addHandOrders.fulfilled, (state, action) => {
+            state.data = action.payload;
+            state.status = 'fulfilled';
+        });
+        builder.addCase(addHandOrders.rejected, (state, action) => {
+            state.data = action.payload;
+            state.status = 'rejected';
+        });
+    },
 });
 
 export const ordersReducer = ordersSlice.reducer;
